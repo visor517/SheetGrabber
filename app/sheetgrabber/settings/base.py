181126be4d2622b55ea38c7d463eda9a0d30534d
visor517 <visor517@yandex.ru>
2022-06-26 21:02:32 +0300

    added celery in docker-compose

--- app/sheetgrabber/settings/base.py
+++ app/sheetgrabber/settings/base.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 
 CELERY_RESULT_BACKEND = 'django-db'
 
